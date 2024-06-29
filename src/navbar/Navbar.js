084c23bb605ddd97dc998ec14f85b67abcd7f9bd
SteveDev1 <mobemobesh28@gmail.com>
2024-06-29 18:56:59 +0300

    main

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -128,6 +128,9 @@ function Nav({ isCartVisible, setIsCartVisible }) {
             <Link to="/womenwear" className="no-underline">Womenwear</Link>
           </li>
           <li className='p-3'>
+            <Link to="/all" className="no-underline">Pages</Link>
+          </li>
+          <li className='p-3'>
             <Link to="/pages" className="no-underline">Pages</Link>
           </li>
           <li className='p-3'>
