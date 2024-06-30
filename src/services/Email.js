67794f2dfd1a34b6da2818edcb8cf3d157e0e02e
SteveDev1 <mobemobesh28@gmail.com>
2024-06-30 17:33:39 +0300

    main

--- src/services/Email.js
+++ src/services/Email.js
@@ -33,7 +33,7 @@ export const ContactUs = () => {
         process.env.REACT_APP_SERVICE_ID,
         process.env.REACT_APP_TEMPLATE_ID,
         form.current,
-        process.env.REACT_APP_PUBLIC_KEY
+        process.env.REACT_APP_PUBLIC_ID
       )
       .then(
         () => {
